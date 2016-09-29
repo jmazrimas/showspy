3f484d09a2f5e7f6fabdf89766544d45ad547cdb
jmazrimas <joe.mazrimas@gmail.com>
2016-09-29 13:22:09 -0500

    first user test in place

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "can be created with uid, access_token and refresh_token" do
+    uid = 'abc123'
+    access_token = 'atok'
+    refresh_token = 'rtok'
+
+    user = User.find_or_create_by(uid: uid)
+    user.access_token = access_token
+    user.refresh_token = refresh_token
+
+    expect(user.save).to eq(true)
+
+  end
 end
