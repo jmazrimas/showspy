674c727538b03a3368f321a3e55db62eb1f90203
jmazrimas <joe.mazrimas@gmail.com>
2016-09-30 11:09:30 -0500

    added address back

--- db/migrate/20160930155923_create_venues.rb
+++ db/migrate/20160930155923_create_venues.rb
@@ -2,6 +2,7 @@ class CreateVenues < ActiveRecord::Migration[5.0]
   def change
     create_table :venues do |t|
       t.string :name
+      t.string :address
       t.integer :jambase_id
       t.timestamps
     end
