63f9ef93dc8a6dfb7c8e2b19117128a02a2fce2b
jmazrimas <joe.mazrimas@gmail.com>
2016-09-30 11:05:44 -0500

    added address to venue model

--- db/schema.rb
+++ db/schema.rb
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20160930155923) do
 
   create_table "venues", force: :cascade do |t|
     t.string   "name"
+    t.string   "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
