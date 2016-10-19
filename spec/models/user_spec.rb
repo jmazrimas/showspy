1a048d5a1027098d57e3fe761575fb7ba41ef346
jmazrimas <joe.mazrimas@gmail.com>
2016-10-19 12:36:40 -0500

    tests added for user genre profile and genre scoring

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  let(:artist1) {Artist.new(name:"BluesBand", genre_list: ["blues", "rock", "hipster"])}
+  let(:artist2) {Artist.new(name:"RapRock", genre_list: ["rock", "hip-hop"])}
+  let(:user1) {
+    u = User.new(uid: "xyz")
+    u.artists << artist1
+    u.artists << artist2
+    u
+  }
+
   it "can be created with uid, access_token and refresh_token" do
     uid = 'abc123'
     access_token = 'atok'
@@ -13,4 +22,15 @@ RSpec.describe User, type: :model do
     expect(user.save).to eq(true)
 
   end
+
+  it "has a genre profile" do
+    expect(user1.listening_genre_profile).to eq({"blues"=>1, "hip-hop"=>1, "hipster"=>1, "rock"=>2})
+  end
+
+  it "is able to score track genres against it's own" do
+    expect(user1.score_track_genres(["rock","country"])).to eq(40)
+  end
+
+  
+
 end
